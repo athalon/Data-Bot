4082702c2954d5819f2bc621b88fde87f918b6bc
athalon <emmettboehden@gmail.com>
2021-03-27 23:59:26 +0100

    Added Dev cog and basic db manipulation commands

---
+++ cogs/Dev.py
@@ -0,0 +1,24 @@
+from discord.ext import commands
+from core.src.helperFunctions.helperFunctions import *
+from replit import db
+
+class DevCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    async def cog_check(self, ctx):
+        # Check if user is the dev (ath, aka. me)
+        return ctx.author.id == 615608898279309312
+    
+    @commands.command()
+    async def db_dump(self, ctx):
+        await ctx.send(str(db.keys()))
+    
+    @commands.command()
+    async def db_del(self, ctx, key):
+        deleteDbEntry(key)
+        await ctx.send(f"Key `{key}` has been deleted!")
+
+
+def setup(bot):
+    bot.add_cog(DevCommands(bot))
